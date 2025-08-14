114f6c28d383dd566a1cf2f956321f8bb823fd19
samuel12-dot <samuelogunduyile76@gmail.com>
2025-08-14 13:23:43 +0100

    feat: Sorting methods

--- sort.js
+++ sort.js
@@ -1,1 +1,22 @@
 //Sorting an array
+
+/* const str = "the quick brown fox jumps over the lazy dog".split(' ')
+console.log(str)
+
+str.sort()
+console.log(str)
+str.sort().reverse()
+console.log(str)
+str.sort((a,b) => a - b)
+console.log(str)
+str.sort((a,b) => a.length - b.length)
+console.log(str) */
+
+//Working with numbers
+
+const arr = [23, 45, 6, 7, 8, 9, 876, 54, 5678, 9]
+arr.sort()
+console.log(arr)
+
+arr.sort((f,g) => f - g)
+console.log(arr)
